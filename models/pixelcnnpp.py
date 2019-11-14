ad33417e9a9fec5794dc8eb53ee1a5da12a8950c
Yuanyuan Ana Shen <yuashe@microsoft.com>
2019-11-15 07:03:07 +0800

    Modify pixelcnnpp.py

--- models/pixelcnnpp.py
+++ models/pixelcnnpp.py
@@ -217,18 +217,16 @@ class nin(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=dim_in,out_channels=dim_out,kernel_size=1)
 
     def forward(self, x):
-        # TODO replace this by 1x1 conv
-        # og_x = x
+        og_x = x
         # assumes pytorch ordering
         """ a network in network layer (1x1 CONV) """
 
-        #x = x.permute(0, 2, 3, 1)
-        #shp = [int(y) for y in x.size()]
-        #out = self.lin_a(x.contiguous().view(shp[0] * shp[1] * shp[2], shp[3]))
-        #shp[-1] = self.dim_out
-        #out = out.view(shp)
-        #return out.permute(0, 3, 1, 2)
-        return self.conv2(x)
+        x = x.permute(0, 2, 3, 1)
+        shp = [int(y) for y in x.size()]
+        out = self.lin_a(x.contiguous().view(shp[0] * shp[1] * shp[2], shp[3]))
+        shp[-1] = self.dim_out
+        out = out.view(shp)
+        return out.permute(0, 3, 1, 2)
 
 
 class down_shifted_conv2d(nn.Module):
